6b11e184688652e235c3bd7aff8cf9e46e0643ed
h1jun <dittoavec@gmail.com>
2021-07-30 01:43:01 +0900

    Fix profile dropdown menu

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -2,5 +2,5 @@ const profile = document.querySelector('.profile');
 const profileDropdown = document.querySelector('.profile-dropdown');
 
 profile.addEventListener('click', () => {
-    profileDropdown.classList.toggle('show');
+    profileDropdown.classList.toggle('hidden');
 })
